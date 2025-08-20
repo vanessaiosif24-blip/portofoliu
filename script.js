253a738b0fc2e4c9d9a17818111a08aace9e4fcd
vanessaiosif24-blip <vanessa.iosif24@gmail.com>
2025-08-20 15:54:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mic efect scroll smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
